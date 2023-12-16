b63c72ebf35b93a07055d69829fbe3cdf168c0e6
Mahdi Khoshzat <92544225+Mahdikhoshzat@users.noreply.github.com>
2023-12-16 14:07:35 +0330

    clean code for main page

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -17,11 +17,15 @@ const Main = () => {
 
   return (
     <Box sx={{display: "flex", justifyContent: "center", alignItems: "center",height: "100%",width: "100%"}}>
-                <Box sx={{display: "flex",flexDirection:"column",justifyContent: "space-between", alignItems: "center",height: "500px",width: "500px",border: "1px solid #ccc",borderRadius: "5px"}}>
-                  <Box>Welcome To Our Bookshob</Box>
-                  <Box></Box>
-                  <Button variant='contained' color='primary' onClick={companyServiceHandler} >Company</Button>
-                  <Button variant='contained' color='primary' onClick={userServiceHandler} >User</Button>
+                <Box sx={{display: "flex",flexDirection:"column",justifyContent: "space-around",height: "500px",width: "500px",border: "1px solid #ccc",borderRadius: "5px"}}>
+                  <Box sx={{display: "flex", justifyContent: "center",alignItems: "center"}}>
+                  <Box sx={{fontSize: "24px", fontWeight: "bold"}}>Welcome To Our Bookshob</Box>
+                  </Box>
+                  <Box sx={{display: "flex", justifyContent: "space-around",alignItems: "center"}}>
+                  <Box sx={{fontSize: "20px",}}>choose your role: </Box>
+                  <Button variant='contained' color='primary' onClick={companyServiceHandler} sx={{width: "105px"}} >Company</Button>
+                  <Button variant='contained' color='primary' onClick={userServiceHandler} sx={{width: "105px"}}>User</Button>
+                  </Box>
                 </Box>
             </Box>
   )
