4740f4ed946d41ba974ce0abb26bbe612bfb3dcd
h.khani <khanihasan1381@gmail.com>
2023-12-25 17:00:39 +0330

    Create new models for book and etc.

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from book.models import Book
 
 
 # Create your models here.
@@ -10,6 +13,8 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=100, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    favorite_book = models.ManyToManyField(Book, verbose_name=_('favorite-book'), related_name='favorite_users')
+    books = models.ManyToManyField(Book, verbose_name=_('books'), related_name='users')
 
     def __str__(self):
         return self.email
--- backend/book/models.py
+++ backend/book/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from publisher.models import Publisher
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=64, verbose_name=_('title'))
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64, verbose_name=_('title'))
+    price = models.IntegerField(verbose_name=_('price'), validators=[MinValueValidator(0)])
+    categories = models.ManyToManyField(Category, verbose_name=_('categories'))
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, verbose_name=_('publisher'))
+
+    class Meta:
+        verbose_name = _('book')
+        verbose_name_plural = _('books')
+
+    def __str__(self):
+        return self.title
