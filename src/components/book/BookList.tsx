ed8d08da7fc5c168070deda0e49414f1cd97df9d
Mahdikhoshzat <mahdikhoshzat2003@gmail.com>
2023-12-29 15:36:15 +0330

    making book list component

--- src/components/book/BookList.tsx
+++ src/components/book/BookList.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BookItem from "./BookItem";
+import AxiosInstance from "../../custom/axios/axiosInstance";
 
-const BookList = () => {
+const ContactList = () => {
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await AxiosInstance.get("/books");
+        const fetchedBooks = response?.data
+        setBooks(fetchedBooks)
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    })();
+  }, []);
+
   return (
     <div>
-      
+      <h2>Book List</h2>
+      <ul>
+        {books?.map((book:any) => (
+          <BookItem key={book?.id} book={book} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default BookList
+export default ContactList;
