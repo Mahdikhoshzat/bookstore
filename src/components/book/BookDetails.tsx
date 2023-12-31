bde027c98bb55ce8784cd365486a882b6b8940b5
Mahdikhoshzat <mahdikhoshzat2003@gmail.com>
2023-12-31 09:27:05 +0330

    style of book details

--- src/components/book/BookDetails.tsx
+++ src/components/book/BookDetails.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import  AxiosInstance  from "../../custom/axios/axiosInstance";
 import MyFakeData from "./myFakeData.json"
+import { Box } from "@mui/material";
+import MyImage from "../../assets/img.jfif"
 
 type MyParams = {
   id: string;
@@ -27,20 +29,29 @@ const BookDetails = () => {
             console.error("Error fetching book details:", error);
           }
     })();
-   }, []);
-
+  }, []);
 
   return (
     <div>
       <h2>Book Details</h2>
       {book ? (
-        <div>
-          <p>
+        <Box>
+          <Box>
+            <Box sx={{height: "250px", width: "350px"}} component='img' src={MyImage} alt="book photo" />
+          </Box>
+          <Box>
+            <Box>
+              <Box>{book.name}</Box>
+              <Box>My Icon</Box>
+            </Box>
+            <Box>{book.summary}</Box>
+          </Box>
+          {/* <p>
             Name: {book.name} {book.summary}
           </p>
           <p>Phone: {book.photo}</p>
-          <p>Category: {book.category}</p>
-        </div>
+          <p>Category: {book.category}</p> */}
+        </Box>
       ) : (
         <p>Loading...</p>
       )}
